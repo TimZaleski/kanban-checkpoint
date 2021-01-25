246580e195d0f4d05f46437c4ca7b0f180797a24
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-01-25 11:37:10 -0700

    init

---
+++ kanban-checkpoint.client/docs/js/chunk-2d0c19e3.c97851f1.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c19e3"],{"478b":function(e,n,t){"use strict";t.r(n);var a=t("7a23");const c={class:"about"},u=Object(a["g"])("h1",null,"This is the about page",-1);function o(e,n,t,o,s,b){return Object(a["p"])(),Object(a["e"])("div",c,[u])}var s={name:"AboutPage"};s.render=o;n["default"]=s}}]);
---
+++ kanban-checkpoint.client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view v-slot="{Component}">
+      <transition name="route" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </main>
+  <footer>
+    <div class="bg-dark text-light text-center p-4">
+      Made with 💖 by CodeWorks
+    </div>
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+export default {
+  name: 'App',
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+</style>
