5c05352a12908796088ed0275a74729083c14edc
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-01-26 11:33:30 -0700

    kanbanlist first pass

--- kanban-checkpoint.client/src/components/KanbanListItemComponent.vue
+++ kanban-checkpoint.client/src/components/KanbanListItemComponent.vue
@@ -1,13 +1,59 @@
+/* eslint-disable */
 <template>
-  <h1>KanbanListItem</h1>
+<router-link :to="{name: 'KanbanDetails', params: {id: kanbanProp.id}}">
+  <div class="col-3">
+    <h2>{{kanbanProp.title}}</h2>
+  </div>
+</router-link>
 </template>
 
 <script>
+import { computed, reactive } from 'vue'
+import { logger } from '../utils/Logger'
+import { AppState } from '../AppState'
+import { kanbanService } from '../services/KanbanService'
 export default {
-
+  name: 'KanbanListItem',
+  props: {
+    kanbanProp: { type: Object, required: true }
+  },
+  setup(props) {
+    const state = reactive({
+    })
+    return {
+      state,
+      deleteBlog() {
+        try {
+          kanbanService.delete(props.kanbanProp.id)
+        } catch (error) {
+          logger.error(error)
+        }
+      },
+      selectKanban () {
+        try {
+          kanbanService.getById(props.kanbanProp.id)
+        }
+        catch (error) {
+          logger.error(error)
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.BlogComponent{
+  border-style: solid;
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
 
+.BlogComponent :hover
+{
+  color: blue;
+}
 </style>
