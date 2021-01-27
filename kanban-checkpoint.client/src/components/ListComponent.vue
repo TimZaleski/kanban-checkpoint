3ec6a7eb03544c22a1863fc4ffc4d20b58ba3f07
Tim Zaleski <zaleskitimothy@yahoo.com>
2021-01-27 10:33:10 -0700

    list start

--- kanban-checkpoint.client/src/components/ListComponent.vue
+++ kanban-checkpoint.client/src/components/ListComponent.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>List</h1>
+  <div class="col-2 min-vh-100 chalk-border">
+    <h1>List</h1>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,8 @@ export default {
 </script>
 
 <style>
-
+.chalk-border {
+  border: 25px solid rgba(255,255,255,.8);
+  border-image: url("https://www.unicefusa.org/sites/default/files/answer-box.png") 20;
+}
 </style>
